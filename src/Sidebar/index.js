cd1247e1c82fb58eac5ce2e8cdfe8d5b24b8544a
Marco Tsang <hkmarcoot@gmail.com>
2022-01-19 19:26:06 +0000

    fix warning bugs

--- src/Sidebar/index.js
+++ src/Sidebar/index.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
-import './sidebar.css';
+//import React, { useState } from "react";
+import React from "react";
+import "./sidebar.css";
 
 // import {
 //   BrowserRouter as Router,
@@ -8,36 +9,32 @@ import './sidebar.css';
 //   Link
 // } from "react-router-dom";
 
-function Sidebar( { newData } ) {
-    console.log(newData)
-    const [focus, setFocus] = useState(false);
+function Sidebar({ newData }) {
+  console.log(newData);
+  //const [focus, setFocus] = useState(false);
 
+  // function focusLink() {
+  //     setFocus(!focus);
+  // }
 
-    function focusLink() {
-        setFocus(!focus);
-    }
+  // let week = newData.map(week => <Link key={week.id} to={`/week${week.id}`} className={!focus ? "link-week": "link-active"} onClick={focusLink}>`Week ${week.id} - ${week.topic}`</Link>);
 
-    // let week = newData.map(week => <Link key={week.id} to={`/week${week.id}`} className={!focus ? "link-week": "link-active"} onClick={focusLink}>`Week ${week.id} - ${week.topic}`</Link>);
-
-    return(
-        <div className="sidebar">
-            {Object.values(newData).map((week) => 
-        (<p key={week.id} className="link-week">Week {week.id} - {week.topic}</p>))}
-
-            
-           
-            
-            
-           
-        </div>
-    )
+  return (
+    <div className="sidebar">
+      {Object.values(newData).map((week) => (
+        <p key={week.id} className="link-week">
+          Week {week.id} - {week.topic}
+        </p>
+      ))}
+    </div>
+  );
 }
 
 export default Sidebar;
 
-{/* <Router>
+/* <Router>
             
             <Link to='/weeks/1' className={!focus ? "link-week": "link-active"} onClick={focusLink}>Week 1 - Foundation</Link>
            
            <Link to='/weeks/3' className="link-week">Week 3 - Front End</Link>
-           </Router> */}
+           </Router> */
